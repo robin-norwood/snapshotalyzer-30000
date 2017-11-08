2dd6c03bf4248c87f9ad592fe49ca2d5003d611c
Robin Norwood <robin@acloudguru.net>
2017-11-08 14:50:32 -0600

    Wait for instances to stop and start during snapshot process

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -83,11 +83,22 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
+
         i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
-            print("Creating snapshot of {0}".format(v.id))
+            print("  Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
 
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done!")
+    
     return
 
 @instances.command('list')
