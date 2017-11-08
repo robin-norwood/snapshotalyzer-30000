67199e4e1663a8df9ee30438d7fb204f050255cb
Robin Norwood <robin@acloudguru.net>
2017-11-08 15:09:49 -0600

    Show all snapshots in list if --all is given

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -27,7 +27,9 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
     help="Only snapshots for project (tag Project:<name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True,
+    help="List all snapshots for each volume, not just the most recent")
+def list_snapshots(project, list_all):
     "List EC2 snapshots"
 
     instances = filter_instances(project)
@@ -43,6 +45,8 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+
+                if s.state == 'completed' and not list_all: break
 
     return
 
